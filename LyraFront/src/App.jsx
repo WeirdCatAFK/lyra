23e58c02557eacfaf08750a2997db32788f3fac5
AngelCastt <acastrejon08@alumnos.uaq.mx>
2026-04-17 15:41:57 -0600

    front de registrar y login

--- LyraFront/src/App.jsx
+++ LyraFront/src/App.jsx
@@ -1,17 +1,33 @@
-import { useState } from 'react'
-import { ExerciseInterface } from './components/exercise/ExerciseInterface.jsx'
-import { ExerciseResultOverlay } from './components/exercise/ExerciseResultOverlay.jsx'
-import HomePage from './components/HomePage.jsx'
-import { useExerciseSession } from './hooks/useExerciseSession.js'
-import { EXERCISES } from './lib/exercises.js'
+import { useState } from "react";
+import { ExerciseInterface } from "./components/exercise/ExerciseInterface.jsx";
+import { ExerciseResultOverlay } from "./components/exercise/ExerciseResultOverlay.jsx";
+import HomePage from "./components/HomePage.jsx";
+import { useExerciseSession } from "./hooks/useExerciseSession.js";
+import { EXERCISES } from "./lib/exercises.js";
+import RegisterScreen from "./components/RegisterPage.jsx";
+import LoginScreen from "./components/LoginPage.jsx";
 
 export default function App() {
-  const [page, setPage] = useState('home')
-  const { currentConfig, index, lastResult, isTransitioning, advance, jumpTo } =
-    useExerciseSession(EXERCISES)
+  const [page, setPage] = useState("login");
 
-  if (page === 'home') {
-    return <HomePage onStart={() => setPage('exercise')} />
+  const { currentConfig, index, lastResult, isTransitioning, advance, jumpTo } =
+    useExerciseSession(EXERCISES);
+
+  if (page == "login") {
+    return (
+      <LoginScreen
+        onNavigateToRegister={() => setPage("register")}
+        onNavigateToHome={() => setPage("home")}
+      />
+    );
+  }
+
+  if (page === "register") {
+    return <RegisterScreen onNavigateToHome={() => setPage("home")} />;
+  }
+
+  if (page === "home") {
+    return <HomePage onStart={() => setPage("exercise")} />;
   }
 
   return (
@@ -26,5 +42,5 @@ export default function App() {
         <ExerciseResultOverlay result={lastResult} onNext={advance} />
       )}
     </div>
-  )
+  );
 }
